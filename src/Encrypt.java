996979ba03e3a267cf639904078dd88aa7fef560
Rallve <77719378+Rallve@users.noreply.github.com>
2022-09-08 13:44:33 +0200

    kryptering funkar typ filläsning fattar jag inte överhuvudtaget

--- src/Encrypt.java
+++ src/Encrypt.java
@@ -1,20 +1,45 @@
 import javax.swing.*;
+import java.util.*;
 
 public class Encrypt {
     public static void main(String[] args) {
+        //List<String> log = new ArrayList<String>();
+        String message = "";
         String input = JOptionPane.showInputDialog("Skriv ett meddelande");
+        char key = JOptionPane.showInputDialog("Ange nyckel").charAt(0);
+        System.out.println(key);
         for (int i = 0; i < input.length(); i++) {
             char letter = input.charAt(i);
             int ascii = letter;
-            String bin = Integer.toBinaryString(ascii);
+            //String keyBin = Integer.toBinaryString(key);
+
+            //String bin = Integer.toBinaryString(ascii);
+            //String hex = Integer.toHexString(ascii);
+
+            int enc = ascii^key;
+
+            //System.out.println(enc);
+
+            char encChar = (char) enc;
+            //System.out.println(encChar);
+
+            message += encChar;
+
+            /*
+            log.add("");
 
             System.out.println(letter);
             System.out.println(ascii);
             System.out.println(bin);
+            System.out.println(hex);
+            System.out.println(keyBin);
+            System.out.println(enc);
+            System.out.println(dec);
+            System.out.println("");
 
-            for (int n = 0; n < bin.length(); n++) {
-
-            }
+             */
         }
+        JOptionPane.showMessageDialog(null, message);
+        System.out.println(message);
     }
 }
---
+++ src/Filläsning.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.DataOutput;
+import java.io.*;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class Filläsning {
+    public static void main(String[] args) {
+        BufferedReader inFile = openTextFileForReading("info.txt");
+        ArrayList<String> fileContent = readAllText(inFile);
+        DataOutputStream outFile = openBinaryFileForWriting("info.dat");
+        writeTextAsBinary(fileContent, outFile);
+    }
+
+    private static DataOutputStream openBinaryFileForWriting(String fileName) {
+        DataOutputStream outFile;
+        try {
+            outFile = new DataOutputStream(new FileOutputStream(fileName));
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to open file for writing, using terminal instead.");
+            outFile = new DataOutputStream(new OutputStream(System.out)
+        }
+    }
+
+    private static ArrayList<String> readAllText(BufferedReader inFile) {
+        ArrayList<String> text = new ArrayList<>();
+        try {
+            String line = inFile.readLine();
+            while (line != null) {
+                text.add(line);
+                line = inFile.readLine();
+            }
+
+        } catch () {
+
+        }
+        return null;
+    }
+
+    private static void writeTextAsBinary(ArrayList<String> fileContent) {
+        for (int i = 0; i < fileContent.size(); i++) {
+
+        }
+    }
+
+    private static BufferedReader openTextFileForReading(String fileName) {
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new FileReader(file));
+        } catch (FileNotFoundException e) {
+
+        }
+        return null;
+    }
+}
